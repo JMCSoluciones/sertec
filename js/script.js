61c75cc8de62c29e8583b52cd3f318818904e3a8
Josue Martinez <josue.mart@jmcsoluciones.com>
2023-01-05 10:40:46 -0600

    Se corrige tamaño de carousel de nuestros servicios en la seccion de servicios

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var salto = document.querySelector("#salto");
+// salto.innerHTML = "<p>¿QUIÉNES <strong>SOMOS?</strong></p>"
+// salto.classList = "banner-title";
+const width = window.screen.width;
+
+if(width >=1100){
+   var texto = salto.textContent
+   var sinSalto = texto.split("\n").join("");
+  console.log(texto)
+  salto = innerText(sinSalto);
+
+}
